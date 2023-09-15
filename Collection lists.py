2121bef46fa92ccba35aa656e3a053a94a5b925f
NagawaEsther <142291818+NagawaEsther@users.noreply.github.com>
2023-09-15 21:10:00 +0300

    Add files via upload

---
+++ Collection lists.py
@@ -0,0 +1,36 @@
+# creating lists
+student_marks=[60,80,90,98]
+
+#change list items
+student_marks=[60,80,90,98]
+student_marks[0:4]=[89]
+print(student_marks)
+
+#Adding list
+student_marks=[60,89,90,98]
+student_marks.append(55)
+print(student_marks)
+
+#Finding size of list
+print(len(student_marks))
+
+#Summation of student_marks
+student_marks=(60+89+90+98)
+print(student_marks)
+
+#check list
+Z= input("Enter items")
+X=input("Enter items")
+print("True , there is a common item in z and x")
+
+
+
+
+     
+
+
+
+
+
+
+
